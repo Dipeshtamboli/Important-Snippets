16fd6b710e66f3c220810989b21574df5444b4cb
Dipesh Tamboli <dipeshtamboli@gmail.com>
2020-07-05 14:20:23 +0530

    auto directory making

--- mnist_download/download_and_save_mnist_jpgs.py
+++ mnist_download/download_and_save_mnist_jpgs.py
@@ -23,13 +23,11 @@ for i in tqdm(range(len(training_data[1]))):
 	img = np.reshape(img,(28,28,1))
 	lab = training_data[1][i]
 	cv2.imwrite(f"train/{lab}_{i}.jpg",img*255)
-f.close()
 for i in tqdm(range(len(validation_data[1]))):
 	img = validation_data[0][i]
 	img = np.reshape(img,(28,28,1))
 	lab = validation_data[1][i]
 	cv2.imwrite(f"val/{lab}_{i}.jpg",img*255)
-f.close()
 for i in tqdm(range(len(test_data[1]))):
 	img = test_data[0][i]
 	img = np.reshape(img,(28,28,1))
